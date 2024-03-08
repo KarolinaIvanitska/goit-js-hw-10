603af4112fede06adc09509673ebbacd365f7774
Karolina Zaiats <karolina.ivanitska@gmail.com>
2024-03-08 21:51:42 +0100

    Summary

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,2 +1,38 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const delay = +e.currentTarget.elements.delay.value;
+  const radioBtn = e.currentTarget.elements.state.value;
+
+  createPromise(radioBtn, delay)
+    .then(res => {
+      iziToast.success({
+        message: `✅ Fulfilled promise in ${delay}ms`,
+      });
+    })
+    .catch(error => {
+      iziToast.error({
+        message: `❌ Rejected promise in ${delay}ms`,
+      });
+    });
+
+  e.currentTarget.reset();
+}
+
+function createPromise(btn, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (btn === 'fulfilled') {
+        resolve({ delay });
+      } else {
+        reject({ delay });
+      }
+    }, delay);
+  });
+}
